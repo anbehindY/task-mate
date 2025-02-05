e2644d8ff4acc3ac12dcf52edd53344ee5ee9ae9
anbehindY <yanminthwin@gmail.com>
2025-02-06 03:26:19 +0700

    Implement dynamic form

---
+++ components/tasks/DynamicForm.tsx
@@ -0,0 +1,217 @@
+import { TaskArrayType } from "@/types";
+import { AddCircleOutline, DeleteForeverOutlined } from "@mui/icons-material";
+import {
+  Box,
+  Button,
+  FormControl,
+  FormControlLabel,
+  FormHelperText,
+  FormLabel,
+  InputLabel,
+  MenuItem,
+  Radio,
+  RadioGroup,
+  Select,
+  TextField,
+} from "@mui/material";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { FieldArray, Form, FormikProps, getIn } from "formik";
+
+type DynamicFormProps = {
+  formikProps: FormikProps<TaskArrayType>;
+};
+
+export default function DynamicForm({ formikProps }: DynamicFormProps) {
+  const {
+    values,
+    handleBlur,
+    handleChange,
+    touched,
+    errors,
+    setFieldValue,
+    resetForm,
+  } = formikProps;
+
+  return (
+    <Form className="w-full mt-4 relative">
+      <FieldArray name="tasks">
+        {({ push, remove }) => (
+          <Box className="grid grid-cols-2 gap-x-4 gap-y-24 place-items-center">
+            {values.tasks.map((_, index) => (
+              <Box key={index} className="flex flex-col gap-8 w-full max-w-md">
+                <TextField
+                  fullWidth
+                  id={`tasks.${index}.taskName`}
+                  name={`tasks.${index}.taskName`}
+                  label="Task Name"
+                  value={values["tasks"][index].taskName}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  error={
+                    getIn(touched, `tasks.${index}.taskName`) &&
+                    Boolean(getIn(errors, `tasks.${index}.taskName`))
+                  }
+                  helperText={
+                    getIn(touched, `tasks.${index}.taskName`) &&
+                    getIn(errors, `tasks.${index}.taskName`)
+                  }
+                />
+                <TextField
+                  fullWidth
+                  id={`tasks.${index}.assignee`}
+                  name={`tasks.${index}.assignee`}
+                  label="Assignee"
+                  value={values.tasks[index].assignee}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  error={
+                    getIn(touched, `tasks.${index}.assignee`) &&
+                    Boolean(getIn(errors, `tasks.${index}.assignee`))
+                  }
+                  helperText={
+                    getIn(touched, `tasks.${index}.assignee`) &&
+                    getIn(errors, `tasks.${index}.assignee`)
+                  }
+                />
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DateTimePicker
+                    name={`tasks.${index}.dueDate`}
+                    value={values.tasks[index].dueDate}
+                    label="Due Date"
+                    format="DD/MM/YYYY HH:mm"
+                    onChange={(date) =>
+                      setFieldValue(`tasks.${index}.dueDate`, date, true)
+                    }
+                    slotProps={{
+                      textField: {
+                        error:
+                          getIn(touched, `tasks.${index}.dueDate`) &&
+                          Boolean(getIn(errors, `tasks.${index}.dueDate`)),
+                        helperText:
+                          getIn(touched, `tasks.${index}.dueDate`) &&
+                          getIn(errors, `tasks.${index}.dueDate`),
+                      },
+                    }}
+                  />
+                </LocalizationProvider>
+
+                <FormControl
+                  error={
+                    getIn(touched, `tasks.${index}.priority`) &&
+                    Boolean(getIn(errors, `tasks.${index}.priority`))
+                  }
+                >
+                  <InputLabel id={`tasks.${index}.priority`}>
+                    Priority
+                  </InputLabel>
+                  <Select
+                    labelId={`tasks.${index}.priority`}
+                    id={`tasks.${index}.priority`}
+                    name={`tasks.${index}.priority`}
+                    defaultValue="Medium"
+                    value={values.tasks[index].priority}
+                    label="Priority"
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    error={
+                      getIn(touched, `tasks.${index}.priority`) &&
+                      Boolean(getIn(errors, `tasks.${index}.priority`))
+                    }
+                  >
+                    <MenuItem value={"Low"}>Low</MenuItem>
+                    <MenuItem value={"Medium"}>Medium</MenuItem>
+                    <MenuItem value={"High"}>High</MenuItem>
+                  </Select>
+                  <FormHelperText>
+                    {getIn(touched, `tasks.${index}.priority`) &&
+                      getIn(errors, `tasks.${index}.priority`)}
+                  </FormHelperText>
+                </FormControl>
+
+                <FormControl
+                  error={
+                    getIn(touched, `tasks.${index}.status`) &&
+                    Boolean(getIn(errors, `tasks.${index}.status`))
+                  }
+                >
+                  <FormLabel id={`tasks.${index}.status`}>Status</FormLabel>
+                  <RadioGroup
+                    aria-labelledby={`tasks.${index}.status`}
+                    id={`tasks.${index}.status`}
+                    name={`tasks.${index}.status`}
+                    value={values.tasks[index].status}
+                    onChange={handleChange}
+                    row={true}
+                  >
+                    <FormControlLabel
+                      value="To Do"
+                      control={<Radio />}
+                      label="To Do"
+                    />
+                    <FormControlLabel
+                      value="In Progress"
+                      control={<Radio />}
+                      label="In Progress"
+                    />
+                    <FormControlLabel
+                      value="In Review"
+                      control={<Radio />}
+                      label="In Review"
+                    />
+                  </RadioGroup>
+                  <FormHelperText>
+                    {getIn(touched, `tasks.${index}.status`) &&
+                      getIn(errors, `tasks.${index}.status`)}
+                  </FormHelperText>
+                </FormControl>
+                {values.tasks.length > 1 && (
+                  <Button
+                    type="button"
+                    variant="contained"
+                    color="error"
+                    onClick={() => remove(index)}
+                  >
+                    <DeleteForeverOutlined fontSize="large" />
+                  </Button>
+                )}
+              </Box>
+            ))}
+            <Button
+              type="button"
+              variant="outlined"
+              sx={{ borderStyle: "dotted", width: "450px", height: "500px" }}
+              onClick={() =>
+                push({
+                  taskName: "",
+                  assignee: "",
+                  dueDate: null,
+                  priority: "",
+                  status: "",
+                  completed: false,
+                })
+              }
+            >
+              <AddCircleOutline className="mr-4" fontSize="large" />
+              Add more
+            </Button>
+          </Box>
+        )}
+      </FieldArray>
+      <div className="flex justify-end mt-12 pr-10 w-full gap-4">
+        <Button
+          type="button"
+          variant="contained"
+          color="inherit"
+          onClick={() => resetForm()}
+        >
+          Reset
+        </Button>
+        <Button type="submit" variant="contained" color="primary" size="large">
+          Submit
+        </Button>
+      </div>
+    </Form>
+  );
+}
---
+++ components/tasks/NewCreateForm.tsx
@@ -0,0 +1,63 @@
+import { TaskArrayType } from "@/types";
+import { Formik } from "formik";
+import { useRouter } from "next/navigation";
+import { array, date, object, string } from "yup";
+import DynamicForm from "./DynamicForm";
+
+export default function NewCreateForm() {
+  const router = useRouter();
+  const initialValues: TaskArrayType = {
+    tasks: [
+      {
+        taskName: "",
+        assignee: "",
+        dueDate: null,
+        priority: "",
+        status: "",
+        completed: false,
+      },
+    ],
+  };
+
+  const validationSchema = object().shape({
+    tasks: array()
+      .of(
+        object().shape({
+          taskName: string().required("Task name is required"),
+          assignee: string().required("Assignee is required"),
+          dueDate: date()
+            .required("Due date is required")
+            .test({
+              test: (value) => value > new Date(),
+              message: "Due date must be in the future",
+            }),
+          priority: string().required("Priority is required"),
+          status: string().required("Status is required"),
+        })
+      )
+      .min(1, "At least one task is required")
+      .max(10, "Maximum of 10 tasks only"),
+  });
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      validationSchema={validationSchema}
+      onSubmit={(response) => {
+        const tasks = JSON.parse(localStorage.getItem("tasks") || "[]");
+        let id = tasks.length > 0 ? tasks[tasks.length - 1].id + 1 : 1;
+        response.tasks.forEach((task) => {
+          tasks.push({ id: id++, ...task });
+        });
+        localStorage.setItem("tasks", JSON.stringify(tasks));
+        router.push("/tasks");
+      }}
+    >
+      {(formikProps) => (
+        <>
+          <DynamicForm formikProps={formikProps} />
+        </>
+      )}
+    </Formik>
+  );
+}
