b5fff8824fe8c2df34cbae0dec0edcfaf7793b20
anbehindY <yanminthwin@gmail.com>
2025-02-17 22:50:40 +0700

    Update types

--- types/index.ts
+++ types/index.ts
@@ -1,13 +1,14 @@
 import { Dayjs } from "dayjs";
 
 export type TaskType = {
-  id: number;
+  id: string;
   taskName: string;
   assignee: string;
-  dueDate: Dayjs | null;
+  dueDate: string | Dayjs | null;
   priority: string;
   status: string;
   completed: boolean;
+  __v: number;
 };
 
 export type TaskArrayType = { tasks: Omit<TaskType, "id">[] };
@@ -18,3 +19,34 @@ export type StatsCardType = {
   icon: React.ReactElement;
   color: string;
 };
+
+export type CreateTaskResponseType = {
+  message: string;
+  newTasks: TaskType[];
+};
+
+export type GetTasksResponseType = {
+  tasks: TaskType[];
+};
+
+export type UpdateTaskResponseType = {
+  message: string;
+  task: TaskType;
+};
+
+export type DeleteTaskResponseType = {
+  message: string;
+};
+
+type TaskPayloadType = {
+  taskName: string;
+  assignee: string;
+  dueDate: string;
+  priority: string;
+  status: string;
+  completed: boolean;
+};
+
+export type CreateTaskPayloadType = TaskPayloadType;
+
+export type UpdateTaskPayloadType = Partial<TaskPayloadType>;
